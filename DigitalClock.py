63453883618ce395e8cebda690b8049d1d69ec6e
Nimmaravikiran <94212790+Nimmaravikiran@users.noreply.github.com>
2023-10-18 18:25:50 +0530

    DigitalClock.py file has been added

---
+++ DigitalClock.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from time import strftime
+
+root = Tk()
+root.geometry("500x400")
+root.resizable(0, 0)
+root.title("Python Digital Clock")
+Label(root, text="Designed by Ravikiran", font="arial 20 bold").pack(side=BOTTOM)
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    mark.config(text=string)
+    mark.after(1000, time)
+
+
+
+
+mark = Label(root, font=('calibri', 40, 'bold'), pady=150, foreground='black')
+mark.pack(anchor='center')
+time()
+mainloop()
